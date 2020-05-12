e6aef8e31d1ee4bafe0f4c1285608b5f98116a5d
Dominik Lübben <dominiklubben@Dominiks-MBP.fritz.box>
2020-05-12 13:32:42 +0200

    Tabelle laut anforderungen entfernt divs hinzugefügt header,index

--- website_root/index.php
+++ website_root/index.php
@@ -9,22 +9,21 @@
     <br>
     <br>
 
-<div>
-  <div>
-    <table>
-        <tr>
-            <div>  <th>  <a href="search_job.php"> <image src="images/auto.jpg" widht="1000000" height="100" alt="Auto"> </a> </th></div>
-            <div>  <th>  <a href="search_job.php"> <image src="images/garten.jpg" widht="1000000" height="100"> </a> </th></div>
-            <div>  <th>  <a href="search_job.php">  <image src="images/haus.jpg" widht="100" height="100">        </a>    </th></div>
-        </tr>
-        <tr>
-              <div><th>  <a href="search_job.php">  <image src="images/technik.jpg" widht="100" height="100"> </a></th></div>
-              <div><th>  <a href="search_job.php"> <image src="images/vertrag.jpeg" widht="100" height="100"> </a></th></div>
-            <div>  <th>  <a href="search_job.php"> <image src="images/haushalt.jpg" widht="100" height="100"> </a></th></div>
-        </tr>
-    </table>
 
-</div>
+  <div>
+
+
+            <div>    <a href="search_job.php"> <image src="images/auto.jpg" widht="1000000" height="100" alt="Auto"> </a> </div>
+            <div>    <a href="search_job.php"> <image src="images/garten.jpg" widht="1000000" height="100"> </a> </div>
+            <div>    <a href="search_job.php">  <image src="images/haus.jpg" widht="100" height="100">        </a>    </div>
+
+            <div> <a href="search_job.php">  <image src="images/technik.jpg" widht="100" height="100"> </a></div>
+            <div>  <a href="search_job.php"> <image src="images/vertrag.jpeg" widht="100" height="100"> </a></div>
+            <div>   <a href="search_job.php"> <image src="images/haushalt.jpg" widht="100" height="100"> </a></div>
+
+
+
+
 </div>
 
 
