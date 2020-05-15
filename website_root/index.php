52e4a0b818b3f9b0df5571ca83c3db0a415a657b
Fenja Bauer <fenja.bauer@uni-oldenburg.de>
2020-05-15 13:15:37 +0200

    ADD: -Bilder aktualisiert -versucht Tabelle einzupflegen

--- website_root/index.php
+++ website_root/index.php
@@ -2,7 +2,23 @@
 <html lang="de" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <style>
+    .grid-container {
+      display: grid;
+      height: auto;
+      align-content: space-between;
+      grid-template-columns: auto auto auto;
+      grid-gap: 10px;
+    <!-- background-color #a8b8ff  -farbe des logos-->
+      background-color: #989898
+      padding:10px;
+    }
+    .gird-container > div {
+      background-color: #a8b8ff;
+      text-align: center;
+      paddig: 20px 0;
+      font-size : 30px;
+    }
   </head>
   <body>
     <?php include "header.html"; ?>
@@ -10,18 +26,15 @@
     <br>
 
 
-  <div>
-
+  <div class = "grid-container">
 
             <div>    <a href="search_job.php"> <image src="images/auto.jpg" widht="1000000" height="100" alt="Auto"> </a> </div>
             <div>    <a href="search_job.php"> <image src="images/garten.jpg" widht="1000000" height="100"> </a> </div>
             <div>    <a href="search_job.php">  <image src="images/haus.jpg" widht="100" height="100">        </a>    </div>
-
+<div> <a href = "search_job.php"> <image src="images/dienstleistungen.jpg" width = "100" height = "100"> </a> </div>
             <div> <a href="search_job.php">  <image src="images/technik.jpg" widht="100" height="100"> </a></div>
-            <div>  <a href="search_job.php"> <image src="images/vertrag.jpeg" widht="100" height="100"> </a></div>
-            <div>   <a href="search_job.php"> <image src="images/haushalt.jpg" widht="100" height="100"> </a></div>
-
-
+            <div>  <a href="search_job.php"> <image src="images/fitness.jpg" widht="100" height="100"> </a></div>
+            <div>   <a href="search_job.php"> <image src="images/taxi.jpg" widht="100" height="100"> </a></div>
 
 
 </div>
