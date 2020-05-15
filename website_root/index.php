aad3960be66b36e49b394a1afc320f70d52364d6
Fenja Bauer <fenja.bauer@uni-oldenburg.de>
2020-05-15 14:04:55 +0200

    Reparatur

--- website_root/index.php
+++ website_root/index.php
@@ -2,28 +2,24 @@
 <html lang="de" dir="ltr">
   <head>
     <meta charset="utf-8">
-
-
-    <link rel="stylesheet" href="styles/global.css">
-    <title></title>
     <style>
     .grid-container {
       display: grid;
       height: auto;
-      align-content: space-between;
+      align-content: space-around;
       grid-template-columns: auto auto auto;
       grid-gap: 10px;
-    <!-- background-color #a8b8ff  -farbe des logos-->
-      background-color: #989898
-      padding:10px;
+      background-color: #989898;
+      padding:5px;
     }
-    .gird-container > div {
-      background-color: #a8b8ff;
+    .grid-container > div {
+      background-color: #989898;
       text-align: center;
-      paddig: 20px 0;
-      font-size : 30px;
+      paddig: 5px 0;
+      font-size : 20px;
     }
 </style>
+    <link rel="stylesheet" href="styles/global.css">
 
   </head>
   <body>
@@ -37,7 +33,7 @@
             <div>    <a href="search_job.php"> <image src="images/auto.jpg" widht="1000000" height="100" alt="Auto"> </a> </div>
             <div>    <a href="search_job.php"> <image src="images/garten.jpg" widht="1000000" height="100"> </a> </div>
             <div>    <a href="search_job.php">  <image src="images/haus.jpg" widht="100" height="100">        </a>    </div>
-<div> <a href = "search_job.php"> <image src="images/dienstleistungen.jpg" width = "100" height = "100"> </a> </div>
+              <div> <a href = "search_job.php"> <image src="images/dienstleistungen.jpg" width = "100" height = "100"> </a> </div>
             <div> <a href="search_job.php">  <image src="images/technik.jpg" widht="100" height="100"> </a></div>
             <div>  <a href="search_job.php"> <image src="images/fitness.jpg" widht="100" height="100"> </a></div>
             <div>   <a href="search_job.php"> <image src="images/taxi.jpg" widht="100" height="100"> </a></div>
