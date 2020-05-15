3f7d58739284f968849e8921cda3be773b2830c0
Fenja Bauer <fenja.bauer@uni-oldenburg.de>
2020-05-15 20:25:29 +0200

    Man initialisiert nur einmal den Head und den Body, also wurde dies aus Header und Footer rausgenommen. Noch dazu habe ich die Aufteilung im Index angepasst

--- website_root/index.php
+++ website_root/index.php
@@ -2,46 +2,31 @@
 <html lang="de" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <style>
-    .grid-container {
-      display: grid;
-      height: auto;
-      align-content: space-around;
-      grid-template-columns: auto auto auto;
-      grid-gap: 10px;
-      background-color: #989898;
-      padding:5px;
-    }
-    .grid-container > div {
-      background-color: #989898;
-      text-align: center;
-      paddig: 5px 0;
-      font-size : 20px;
-    }
-</style>
-    <link rel="stylesheet" href="styles/global.css">
-
+    <link rel="stylesheet" type="text/css" href="styles/global.css">
+    <link rel="stylesheet" type="text/css" href="styles/global.css">
   </head>
   <body>
+      <div class = "grid-container">
     <?php include "header.html"; ?>
+
     <br>
     <br>
 
 
-  <div class = "grid-container">
 
-            <div>    <a href="search_job.php"> <image src="images/auto.jpg" widht="1000000" height="100" alt="Auto"> </a> </div>
-            <div>    <a href="search_job.php"> <image src="images/garten.jpg" widht="1000000" height="100"> </a> </div>
-            <div>    <a href="search_job.php">  <image src="images/haus.jpg" widht="100" height="100">        </a>    </div>
-              <div> <a href = "search_job.php"> <image src="images/dienstleistungen.jpg" width = "100" height = "100"> </a> </div>
-            <div> <a href="search_job.php">  <image src="images/technik.jpg" widht="100" height="100"> </a></div>
-            <div>  <a href="search_job.php"> <image src="images/fitness.jpg" widht="100" height="100"> </a></div>
-            <div>   <a href="search_job.php"> <image src="images/taxi.jpg" widht="100" height="100"> </a></div>
 
+            <div class="grid-item"> <div class="images">   <a href="search_job.php"> <image src="images/auto.jpg" > </a>
+            <a href="search_job.php"> <image src="images/garten.jpg" > </a>
+       <a href="search_job.php"> <image src="images/haus.jpg" >  </a>
+           <a href="search_job.php"> <image src="images/dienstleistungen.jpg" > </a>
+            <a href="search_job.php">  <image src="images/technik.jpg" ></a>
+           <a href="search_job.php"> <image src="images/fitness.jpg" > </a>
+              <a href="search_job.php"> <image src="images/taxi.jpg" > </a>
 
 </div>
-
-
-    <?php include "footer.html"; ?>
+</div>
+  <?php include "footer.html"; ?>
+</div>
+  
   </body>
 </html>
