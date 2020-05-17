f912610cb23d3c504f1c7af343fb5649c6cf263a
Dominik Lübben <dominiklubben@Dominiks-MBP.fritz.box>
2020-05-17 21:18:08 +0200

    Valiator änderungen

--- website_root/offer_job.php
+++ website_root/offer_job.php
@@ -14,25 +14,25 @@
         <ul class="navi">
           <li class="navibutton"> <a href="index.php" class="naviobjekt"> Startseite</a></li>  <!--active Anzeige nur als TEst wie umsteztbar???-->
         <li class="navibutton">   <a href="search_job.php" class="naviobjekt"> Job suchen </a></li>
-          <li class="navibutton"> <div class="active"> <a href="offer_job.php" class="naviobjekt"> </div>Anzeige erstellen</a></li>
+          <li class="navibutton"> <div class="active"> <a href="offer_job.php" class="naviobjekt"> Anzeige erstellen</a></li>
         <li class="navibutton">   <a href="contact.php" class="naviobjekt">Kontakt </a></li>
           <li class="navibutton"> <a href="impressum.php" class="naviobjekt"> Impressum</a></li></ul>
         </div>
       </nav>
       <div>
-      Anzeigen Name:  <input type="text" placeholder=""></input>
+      Anzeigen Name:  <input type="text" placeholder=""><
       </div>
       <div>
-      Beschreibung:  <input type="text" placeholder=""></input>
+      Beschreibung:  <input type="text" placeholder="">
       </div>
     <div>
-      Hier Bild einfügen <a href="offer_job.php"> <input type="button" value="Bild hochladen"  >  </input>
+      Hier Bild einfügen <a href="offer_job.php"> <input type="button" value="Bild hochladen"  >
     </div>
       <div>
-        <input type="submit" value="Anzeige freischalten"></input>
+        <input type="submit" value="Anzeige freischalten">
       </div>
       <div>
-        <input type="reset" value="Zurücksetzen"></input>
+        <input type="reset" value="Zurücksetzen">
       </div>
       <?php include "footer.html"; ?>
     </div>
