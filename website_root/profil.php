bda651e01e9cc6144beb4eb7a9f10b638b8bf918
Dominik Lübben <dominiklubben@Dominiks-MBP.fritz.box>
2020-05-20 14:22:19 +0200

    Mein Profil erstellt Impressum angepasst

--- website_root/profil.php
+++ website_root/profil.php
@@ -22,15 +22,49 @@
 
     </nav>
 </div>
+<div class="grid-farbe">
 <!--Searchbox-->
 <h1> Mein Profil</h1>
-Beispieltext
-Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
-elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-amet.
-<?php include "footer.html"; ?>
+<div class="container">
+    <div class="contact-row">
+
+        <div class="contact-col">
+            <div>
+
+                <h2 class="footer-h2">Meine Daten</h2>
+                <ul class="list-unstyled">
+                    <li class="footer-li">Vorname: <input type="text" placeholder="Max"></li>
+                    <li class="footer-li">Nachname: <input type="text" placeholder="Mustermann"></li>
+                    <li class="footer-li">Email: <input type="text" placeholder="max.mustermann@123.de"></li>
+                    <li class="footer-li">Straße: <input type="text" placeholder="Musterstraße 12"></li>
+                    <li class="footer-li">Postleizahl: <input type="text" placeholder="12345 Musterstadt"></li>
+                      <li class="footer-li"> <input type="submit" value="Daten sichern"><input type="submit" value="Konto löschen"></li>
+                </ul>
+
+            </div>
+        </div>
+
+
+        <div class="contact-col">
+            <div>
+                <h2 class="footer-h2">Meine Anzeigen</h2>
+                <p>At vero eos et accusam et justo duo dolores et ea rebum.
+                    Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                    invidunt ut labore et dolore magna aliquyam erat,
+                    sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+                    gubergren,
+                    no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+            </div>
+        </div>
+
+    </div>
+</div>
+</form>
+</div>
+
+
+</div><?php include "footer.html"; ?>
 </body>
 </html>
