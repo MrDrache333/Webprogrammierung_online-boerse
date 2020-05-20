caa380e3dda6a396e248b67962e44d2963855fa8
Dominik Lübben <dominiklubben@Dominiks-MBP.fritz.box>
2020-05-20 16:13:49 +0200

    Activ auf profil hinzugefügt

--- website_root/profil.php
+++ website_root/profil.php
@@ -14,8 +14,8 @@
         <ul class="navi">
             <li class="navibutton"><a href="index.php" class="naviobjekt"> Startseite</a></li>
             <!--active Anzeige nur als TEst wie umsteztbar???-->
-
-            <li class="navibutton"><a href="profil.php" class="naviobjekt"> Mein Profil</a></li>
+<li class="navibutton">
+          <div class="active"> <a href="profil.php" class="naviobjekt"> Mein Profil</a></div></li>
             <li class="navibutton"><a href="contact.php" class="naviobjekt">Kontakt </a></li>
             <li class="navibutton"><a href="impressum.php" class="naviobjekt"> Impressum</a></li>
         </ul>
