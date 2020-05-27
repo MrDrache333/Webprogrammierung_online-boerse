fb6b118927d77c69181bf7bfb3783af85e938b65
MrDrache333 <kenoog@freenet.de>
2020-05-27 20:21:28 +0200

    Grundgerüst Ergebnisseite

--- website_root/search_job.php
+++ website_root/search_job.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="styles/global.css">
     <link rel="stylesheet" type="text/css" href="styles/login.css">
+    <link rel="stylesheet" type="text/css" href="styles/search_job.css">
 
     <title>KEFEDO-Job-suchen</title>
 </head>
@@ -29,17 +30,100 @@
     <div class="container">
         <div class="jobs-content">
             <div class="jobs-search">
-                <form>
-
+                <form class="searchBox">
+                    <p class="searchBox-text">Wonach suchen Sie ?</p>
+                    <input class="searchBox-input searchBox-item" type="text"
+                           placeholder="Was? (z.B. Beruf oder Stichwort)"/>
+                    <input class="searchBox-input searchBox-item" type="text" placeholder="Wo? (z.B. PLZ oder Ort)"/>
+                    <a href="search_job.php" class="searchBox-button searchBox-button-text searchBox-item"><span
+                                class="searchBox-button-gradient"></span>Suchen</a>
                 </form>
             </div>
-            <div class="jobs-filter">
-                <form>
-
-                </form>
-            </div>
-            <div class="jobs-result">
-
+            <hr>
+            <div class="jobs-content-dynamic">
+                <div class="jobs-filter">
+                    <form>
+                        <div class="filter-container">
+                            <fieldset class="filter-fieldset">
+                                <h3 class="filter-h3">Angebotsart</h3>
+                                <div class="filter-row">
+                                    <input type="radio" id="f-type-1" name="type">
+                                    <label class="filter-cb-label" for="f-type-1">Arbeit</label>
+                                </div>
+                                <div class="filter-row">
+                                    <input type="radio" id="f-type-2" name="type">
+                                    <label class="filter-cb-label" for="f-type-2">Ausbildung</label>
+                                </div>
+                                <div class="filter-row">
+                                    <input type="radio" id="f-type-3" name="type">
+                                    <label class="filter-cb-label" for="f-type-3">Praktikum</label>
+                                </div>
+                                <div class="filter-row">
+                                    <input type="radio" id="f-type-4" name="type">
+                                    <label class="filter-cb-label" for="f-type-4">Selbstständigkeit</label>
+                                </div>
+                            </fieldset>
+                            <fieldset class="filter-fieldset">
+                                <h3 class="filter-h3">Befristung</h3>
+                                <div class="filter-row">
+                                    <input type="checkbox" id="f-duration-1">
+                                    <label class="filter-cb-label" for="f-duration-1">Befristet</label>
+                                </div>
+                                <div class="filter-row">
+                                    <input type="checkbox" id="f-duration-2">
+                                    <label class="filter-cb-label" for="f-duration-2">Unbefristet</label>
+                                </div>
+                                <div class="filter-row">
+                                    <input type="checkbox" id="f-duration-3">
+                                    <label class="filter-cb-label" for="f-duration-3">Keine Angabe</label>
+                                </div>
+                            </fieldset>
+                            <fieldset class="filter-fieldset">
+                                <h3 class="filter-h3">Arbeitszeitmodell</h3>
+                                <div class="filter-row">
+                                    <input type="checkbox" id="f-time-1">
+                                    <label class="filter-cb-label" for="f-time-1">Vollzeit</label>
+                                </div>
+                                <div class="filter-row">
+                                    <input type="checkbox" id="f-time-2">
+                                    <label class="filter-cb-label" for="f-time-2">Teilzeit</label>
+                                </div>
+                                <div class="filter-row">
+                                    <input type="checkbox" id="f-time-3">
+                                    <label class="filter-cb-label" for="f-time-3">Schicht</label>
+                                </div>
+                                <div class="filter-row">
+                                    <input type="checkbox" id="f-time-4">
+                                    <label class="filter-cb-label" for="f-time-4">Heim- / Telearbeit</label>
+                                </div>
+                                <div class="filter-row">
+                                    <input type="checkbox" id="f-time-5">
+                                    <label class="filter-cb-label" for="f-time-5">Minijob</label>
+                                </div>
+                            </fieldset>
+                        </div>
+                    </form>
+                </div>
+                <div class="jobs-result">
+                    <article role="article">
+                        <div class="article-content">
+                            <h3 class="result-h3" role="heading">Template</h3>
+                            <span>(SomeJob)</span>
+                        </div>
+                    </article>
+                    <article role="article">
+                        <div class="article-content">
+                            <h3 class="result-h3" role="heading">Template</h3>
+                            <span>(SomeJob)</span>
+                        </div>
+                    </article>
+                    <article role="article">
+                        <div class="article-content">
+                            <h3 class="result-h3" role="heading">Template</h3>
+                            <span>(SomeJob)</span>
+                        </div>
+                    </article>
+                </div>
             </div>
         </div>
     </div>
