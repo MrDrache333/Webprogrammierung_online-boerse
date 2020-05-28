c2f88185672f088530c9cb0b0e67c4a8e081571c
Dominik Lübben <dominiklubben@Dominiks-MBP.fritz.box>
2020-05-28 11:15:47 +0200

    Viewpoint hinzugefügt

--- website_root/index.php
+++ website_root/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="styles/global.css">
     <link rel="stylesheet" type="text/css" href="styles/login.css">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  
 
     <title>KEFEDO-Startseite</title>
 </head>
