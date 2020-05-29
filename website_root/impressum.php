500941bcac7dbde427c03a59d06a6a1a3d984de5
MrDrache333 <kenoog@freenet.de>
2020-05-29 11:42:22 +0200

    Login grob gefixed

--- website_root/impressum.php
+++ website_root/impressum.php
@@ -5,20 +5,16 @@
     <link rel="stylesheet" href="styles/global.css">
     <link rel="stylesheet" href="styles/contact.css">
 
-
     <title>KEFEDO-Impressum</title>
 </head>
 <body>
 <div class="grid-container">
-
     <!--Header einbinden-->
     <?php include "header.html"; ?>
     <div class="navileiste">
-
         <ul class="navi">
             <li class="navibutton"><a href="index.php" class="naviobjekt"> Startseite</a></li>
             <!--active Anzeige nur als TEst wie umsteztbar???-->
-
             <li class="navibutton"><a href="profil.php" class="naviobjekt"> Mein Profil</a></li>
             <li class="navibutton"><a href="contact.php" class="naviobjekt">Kontakt </a></li>
             <li class="navibutton">
@@ -30,60 +26,41 @@
         <h1>Impressum</h1>
         <div class="container">
             <div class="contact-row">
-
                 <div class="contact-col">
-
-
-                        <h2 class="contact-h2">Datenschutzbestimmungen</h2>
-                        <p> At vero eos et accusam et justo duo dolores et ea rebum.
-                            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                            invidunt ut labore et dolore magna aliquyam erat,
-                            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-                            gubergren,
-                            no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                        </p>
-
-
-                </div>
-
-
-                <div class="contact-col">
-
-                        <h2 class="contact-h2">Nutzungsbedingungen</h2>
-                        <p>At vero eos et accusam et justo duo dolores et ea rebum.
-                            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                            invidunt ut labore et dolore magna aliquyam erat,
-                            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-                            gubergren,
-                            no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-
-
+                    <h2 class="contact-h2">Datenschutzbestimmungen</h2>
+                    <p> At vero eos et accusam et justo duo dolores et ea rebum.
+                        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                        invidunt ut labore et dolore magna aliquyam erat,
+                        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+                        gubergren,
+                        no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                    </p>
                 </div>
                 <div class="contact-col">
-
-                        <h2 class="contact-h2">Impressum</h2>
-                        <p>At vero eos et accusam et justo duo dolores et ea rebum.
-                            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                            invidunt ut labore et dolore magna aliquyam erat,
-                            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-                            gubergren,
-                            no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-                        <div>
-
-                    </div>
+                    <h2 class="contact-h2">Nutzungsbedingungen</h2>
+                    <p>At vero eos et accusam et justo duo dolores et ea rebum.
+                        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                        invidunt ut labore et dolore magna aliquyam erat,
+                        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+                        gubergren,
+                        no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div class="contact-col">
+                    <h2 class="contact-h2">Impressum</h2>
+                    <p>At vero eos et accusam et justo duo dolores et ea rebum.
+                        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                        invidunt ut labore et dolore magna aliquyam erat,
+                        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+                        gubergren,
+                        no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
                 </div>
             </div>
         </div>
-        </form>
     </div>
-
-
 </div>
 <?php include "footer.html"; ?>
-
-</div>
 </body>
 </html>
