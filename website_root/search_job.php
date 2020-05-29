63880214fd310f3cd2cfc010f736bc6b8ce85eee
MrDrache333 <kenoog@freenet.de>
2020-05-29 14:04:18 +0200

    Suchbox auf der Startseite wieder anpassen

--- website_root/search_job.php
+++ website_root/search_job.php
@@ -10,10 +10,8 @@
 </head>
 <body>
 <!--Header einbinden-->
-
 <?php include "header.html"; ?>
 <div class="header">
-
     <ul class="navi">
         <li class="navibutton"><a href="index.php" class="naviobjekt"> Startseite</a></li>
         <!--active Anzeige nur als TEst wie umsteztbar???-->
