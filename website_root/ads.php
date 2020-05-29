ab9cb7daf54d2c3118899570662c5af557dc24ef
Dominik Lübben <dominiklubben@Dominiks-MacBook-Pro.local>
2020-05-29 13:59:26 +0200

    impressum repariert

--- website_root/ads.php
+++ website_root/ads.php
@@ -32,17 +32,17 @@
 <div class="row">
   <div class="leftcolumn">
     <div class="card">
+      <div class="anzeigen-inhalt">
       <h2>Verkäufer/-in (m/w/d) <br>
       (Fachverkäufer/-in - Lebensmittelhandwerk)</h2>
-        <img  class="fakeimg"src="images/company_placeholder.png"
-             alt="Firmenlogo">
+        <img  class="fakeimg" src="images/company_placeholder.png" alt="Firmenlogo">
       <div class="beschreibung">
     Hier könnte ihre Werbung stehen.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
 
-    </div>
+    </div></div>
     <div class="infos">
-    <div class="erstellt">  <h5>erstellt am: 08.12.2020   </div>
-    <div class="frei">  <h5> Frei ab: 12.12.2020</h5>    </div>
+    <div class="erstellt">  <h5>erstellt am:<br> 08.12.2020   </div>
+    <div class="frei">  <h5> Frei ab:<br>12.12.2020</h5>    </div>
     <div class="frei"><a href="" class="ads_button" ><h5>bearbeiten</h5>     </a>      </div>
       <div class="frei"><a href="" class="ads_button"><h5>löschen</h5>     </a>   </div>
   </div>
