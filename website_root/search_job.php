d78b0e4a6e53a55197e5d43e201d80cb35c51352
dominiklubben <dominik.luebben@uni-oldenburg.de>
2020-06-03 21:50:06 +0200

    Navileiste in Suchen Seite angepasst

--- website_root/search_job.php
+++ website_root/search_job.php
@@ -2,10 +2,26 @@
 <?php include "header.php"; ?>
 <div class="header">
     <ul class="navi">
-        <li class="navibutton"><a href="index.php" class="naviobjekt"> Startseite</a></li>
-        <!--active Anzeige nur als TEst wie umsteztbar???-->
+        <?php
+        if (isset($_COOKIE["loggedin"]) and $_COOKIE["loggedin"] === "true") { ?>
 
-        <li class="navibutton"><a href="profil.php" class="naviobjekt"> Mein Profil</a></li>
+            <li class="navibutton">
+                <div class="active"><a href="index.php" class="naviobjekt"> Startseite</a></div>
+            </li>
+            <li class="navibutton"><a href="profil.php" class="naviobjekt"> Mein Profil</a></li>
+            <li class="navibutton"><a href="messages.php" class="naviobjekt">Meine Anzeigen </a></li>
+        <?php } else {
+            ?>
+            <li class="navibutton">
+                <div class="active"><a href="index.php" class="naviobjekt"> Startseite</a></div>
+            </li>
+            <li class="navibutton"><a href="index.php" class="naviobjekt"> Mein Profil</a></li>
+
+
+        <?php }
+        ?>
+
+
         <li class="navibutton"><a href="contact.php" class="naviobjekt">Kontakt </a></li>
         <li class="navibutton"><a href="impressum.php" class="naviobjekt"> Impressum</a></li>
     </ul>
