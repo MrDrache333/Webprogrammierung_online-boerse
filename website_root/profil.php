1a039b79e26b53a462678aed23a51ee216c33a4b
MrDrache333 <kenoog@freenet.de>
2020-06-23 19:01:03 +0200

    md5 durch richtige Verschlüsselung ersetzt

--- website_root/profil.php
+++ website_root/profil.php
@@ -85,8 +85,8 @@ if (isset($_POST["pb_submit"])) {
         $uploadOk = 0;
     }
 // Allow certain file formats
-    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif") {
+    if ($imageFileType !== "jpg" && $imageFileType !== "png" && $imageFileType !== "jpeg"
+        && $imageFileType !== "gif") {
         echo "Nur JPG, JPEG, PNG & GIF Dateiformate sind erlaubt.";
         $uploadOk = 0;
     }
